ddafd2769bd4000cde8948c5818fd19541021d3a
EliasMN <elias010493@gmail.com>
2024-01-17 18:25:54 +0100

    Inicial
    Configurando el proyecto y primera ruta al componente lab

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LabComponent } from './pages/lab/lab.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "lab", component: LabComponent }
+];
