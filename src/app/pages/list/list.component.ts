4c0b8e1c3150ee738867441a1d716b46be041971
EliasMN <elias010493@gmail.com>
2024-02-02 17:10:56 +0100

    Clase 8
    Localstorage

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Injector, computed, effect, inject, signal } from '@angular/core';
 import { Tarea } from '../../model/tarea.model';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
@@ -33,8 +33,8 @@ export class ListComponent {
 
   updateModeEditing(id: number) {
     this.listTask.update((listTask) => {
-      return listTask.map((task, posicion) => {
-        if (id === posicion) {
+      return listTask.map(task => {
+        if (id === task.id) {
           return {
             ...task,
             editando: true
@@ -52,8 +52,8 @@ export class ListComponent {
     let texto = event.target as HTMLInputElement;
 
     this.listTask.update((listTask) => {
-      return listTask.map((task, posicion) => {
-        if (id === posicion) {
+      return listTask.map(task => {
+        if (id === task.id) {
           return {
             ...task,
             nombre: texto.value,
@@ -101,13 +101,13 @@ export class ListComponent {
 
   }
 
-  deleteTask(id: Number) {
+  deleteTask(id: number) {
     // Mutar el listado sería usando un método como .splice sobre el array
     // Pero siguiendo el patrón de crear nuevos estados de las señales de Angular
     // Utilizamos la función nativa de JS .filter para quedarnos con las tareas que cumplan una condición, que
     // se basa en comparar el índice del elemento al que hemos pulsado del listado
 
-    this.listTask.update((listTask) => listTask.filter((task, posicion) => posicion !== id));
+    this.listTask.update((listTask) => listTask.filter(task => task.id !== id));
 
     /* Lo mismo pero de otra forma:
     this.listTask.update((listTask) => {
@@ -118,10 +118,10 @@ export class ListComponent {
     */
   }
 
-  updateTask(id: Number) {
+  updateTask(id: number) {
     this.listTask.update((listTask) => {
-      return listTask.map((task, posicion) => {
-        if (id === posicion) {
+      return listTask.map(task => {
+        if (id === task.id) {
           return {
             ...task,
             completada: !task.completada
@@ -145,18 +145,48 @@ export class ListComponent {
 
     // Obtengo las tareas no completadas
     if (filtro === "completadas") {
-      return listado.filter(tarea => tarea.completada === false);
+      return listado.filter(tarea => tarea.completada === true);
     }
 
     // Obtengo las tareas completadas
     if (filtro === "pendientes") {
-      return listado.filter(tarea => tarea.completada === true);
+      return listado.filter(tarea => tarea.completada === false);
     }
 
     // Obtengo todas las tareas
     return listado;
 
   });
+
+  limpiarCompletadas() {
+    this.listTask.update((listTask) => listTask.filter(task => task.completada === false));
+  }
+
+  // Función de Angular que se ejecuta al inicializar el componente
+  ngOnInit() {
+    let datosStorage = localStorage.getItem("listadoTareas");
+    if (datosStorage) {
+      let listaTareas = JSON.parse(datosStorage);
+      this.listTask.set(listaTareas);
+    }
+    this.trackTareas();
+  }
+
+  // La función inject y el elemento Injector de Angular que le asignamos a trackTareas sirve para
+  // que podamos utilizar los effects.
+  tareasInjector = inject(Injector);
+
+  trackTareas() {
+    effect(() => {
+      // Esto se ejecutará ante cualquier cambio, incluida la
+      // inicialización del signal (como crear el array vacío)
+      let listaTareas = this.listTask();
+
+      // Útil para para la persistencia de los datos
+      localStorage.setItem('listadoTareas', JSON.stringify(listaTareas));
+    }, { injector: this.tareasInjector });
+  }
+
 
 
 }
